942d45efc44cac2195c3f979445f98b0adc2cdde
Евгений Колотов <evgenykolotov.dev@gmail.com>
2023-10-04 17:05:15 +0300

    feat: Паттерн проектирования Singleton

---
+++ src/design-patterns/singleton/singleton.ts
@@ -0,0 +1,12 @@
+export default class Singleton {
+    private static instance: Singleton | null = null;
+
+    private constructor() {}
+
+    public static getInstance(): Singleton {
+        if (!Singleton.instance) {
+            Singleton.instance = new Singleton();
+        }
+        return Singleton.instance;
+    }
+}
